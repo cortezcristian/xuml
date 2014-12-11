124cda63040829046cdbe8089bfca0ee244ee37f
Cristian Cortez <cortez.cristian@gmail.com>
2014-12-11 20:15:54 -0300

    Kickstart yo angular

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name xumlApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the xumlApp
+ */
+angular.module('xumlApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name xumlApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the xumlApp
+ */
+angular.module('xumlApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
